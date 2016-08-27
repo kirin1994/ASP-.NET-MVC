2c5b3e4f7ed57b2eb2c94224abce5f18eb0f169f
kirin1994 <apodlawski@gmail.com>
2016-08-27 21:26:56 +0200

    Add name to artist.

--- MvcProject/MvcProject/Controllers/AccountController.cs
+++ MvcProject/MvcProject/Controllers/AccountController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MvcProject.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace MvcProject.Controllers
 {
@@ -151,7 +148,13 @@ namespace MvcProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
